016e77e7aaf314f4411217bc297c56f8c4cdb880
evgjeniy <dashkevich2016@mail.ru>
2024-07-30 19:42:45 +0300

    add slider

--- Assets/Code/UI/Elements/GameHud.cs
+++ Assets/Code/UI/Elements/GameHud.cs
@@ -1,11 +1,21 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameHud : Window
 {
     [SerializeField] private Button pauseButton;
+
+    [Header("Color Tasks")]
     [SerializeField] private Transform colorTasksRoot;
     [SerializeField] private ColorTask colorTaskPrefab;
+
+    [Header("Stack")]
+    [SerializeField] private Slider stackSlider;
+    [SerializeField] private TMP_Text maxAmountText;
+    [SerializeField] private TMP_Text currentAmountText;
+    [SerializeField] private VerticalLayoutGroup stackCubesRoot;
+    [SerializeField] private GameObject linePrefab;
 
     public Button Pause => pauseButton;
 
@@ -16,5 +26,26 @@ public class GameHud : Window
             var colorTaskInstance = Instantiate(colorTaskPrefab, colorTasksRoot);
             colorTaskInstance.Construct(colorPurposeData);
         }
+
+        CreateStackSlider(config);
     }
+
+    private void CreateStackSlider(LevelConfig config)
+    {
+        stackSlider.maxValue = config.MaxStackAmount;
+        stackSlider.value = 0.0f;
+
+        if (stackSlider.transform is RectTransform sliderRect)
+            sliderRect.sizeDelta = new Vector2(sliderRect.sizeDelta.x, config.MaxStackAmount * 100);
+
+        var padding = config.MaxStackAmount * 50 / config.MaxStackAmount;
+        stackCubesRoot.padding.top = stackCubesRoot.padding.bottom = padding;
+
+        for (var i = 1; i < config.MaxStackAmount; i++)
+            Instantiate(linePrefab, stackCubesRoot.transform);
+
+        maxAmountText.text = $"MAX\n{config.MaxStackAmount}";
+    }
+
+    public void OnSliderValueChanged(float value) => currentAmountText.text = $"{value}";
 }
